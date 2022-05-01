84fbfc759fbaf104fc3ff71ad936f8339aa89db6
Kartik <itskwadehra@gmail.com>
2022-05-01 18:06:51 +0530

    First commit

---
+++ src/main/java/com/cpadassignment/rentatrend/RentATrendApplication.java
@@ -0,0 +1,13 @@
+package com.cpadassignment.rentatrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentATrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentATrendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpadassignment/rentatrend/RentATrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpadassignment.rentatrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentATrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
